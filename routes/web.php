1c238b1fdd3cc8d142fc66ea4e73a419f30b5195
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2023-02-11 17:51:10 -0300

    front end
    Gerando graficos com a IA

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
-use App\Models\SalesCommission;
+use App\Http\Controllers\SaleController;
+use App\Http\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
-use OpenAI\Laravel\Facades\OpenAI;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', Dashboard::class)->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -30,21 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('clients', ClientController::class);
+    Route::get('/sales', [SaleController::class, 'index'])->name('sale.index');
 
-    Route::get('/chart', function(){
-
-        $fields = implode(',', SalesCommission::getColumns());
-
-        $question = 'Gere um gráfico das vendas por empresa no eixo y ao longo dos ultimos 5 anos';
-
-        $config =  OpenAI::completions()->create([
-            'model' => 'text-davinci-003',
-            'prompt' => "Considerando a lista de campos ($fields), gere uma configuração json do Vega-lite v5 (sem campo de dados e com descrição) e atenda o seguinte pedido $question. Resposta:",
-            'max_tokens' => 1500,
-        ])->choices[0]->text;
-
-        dd($config);
-    });
 });
 
 require __DIR__.'/auth.php';
